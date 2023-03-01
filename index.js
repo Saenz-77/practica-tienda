87ae06f29e73c5e9d7fa32aaea4d1e04ad01f09e
Saenz-77 <saenzgarzonalejo@gmail.com>
2023-03-01 15:49:48 -0500

    menús desplegables

--- index.js
+++ index.js
@@ -1,8 +1,43 @@
 const displayEmail = document.querySelector('.nav-mail');
 const menuDesk = document.querySelector('.menu-desk');
+const menuHambur = document.querySelector('.menu-principal');
+const mobile = document.querySelector('.mobile');
+const navCar = document.querySelector('.nav-car');
+const products = document.querySelector('.products');
 
 displayEmail.addEventListener('click', manejarMenu);
+menuHambur.addEventListener('click', menuMobile);
+navCar.addEventListener('click', menuProducts);
 
 function manejarMenu(){
-    menuDesk.classList.toggle('inactive')
+    const isProductsClosed = products.classList.contains('.inactive');
+
+    if (!isProductsClosed){
+        products.classList.add('inactive');
+    }
+    menuDesk.classList.toggle('inactive');
+}
+
+function menuMobile(){
+    const isProductsClosed = products.classList.contains('.inactive');
+
+    if (!isProductsClosed){
+        products.classList.add('inactive');
+    }
+    mobile.classList.toggle('inactive');
+}
+
+function menuProducts(){
+    const isMobileMenuClosed = mobile.classList.contains('.inactive');
+    const isMenuEmailClosed = menuDesk.classList.contains('inactive');
+
+
+    if (!isMobileMenuClosed){
+        mobile.classList.add('inactive');
+    }
+    if (!isMenuEmailClosed){
+        menuDesk.classList.add('inactive');
+    }
+    
+    products.classList.toggle('inactive');
 }
