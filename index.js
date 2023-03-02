b928becc933729e91a26e945509b8f74c783f230
Saenz-77 <saenzgarzonalejo@gmail.com>
2023-03-01 22:48:26 -0500

    Añadida lista de productos por medio de JS

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const menuHambur = document.querySelector('.menu-principal');
 const mobile = document.querySelector('.mobile');
 const navCar = document.querySelector('.nav-car');
 const products = document.querySelector('.products');
+const cards = document.querySelector('.cards');
 
 displayEmail.addEventListener('click', manejarMenu);
 menuHambur.addEventListener('click', menuMobile);
@@ -41,3 +42,52 @@ function menuProducts(){
     
     products.classList.toggle('inactive');
 }
+
+const productsList = [];
+productsList.push({
+    name: 'articulo',
+    price: '100.000',
+    img: './logos/innogital.png'
+})
+productsList.push({
+    name: 'articulo',
+    price: '100.000',
+    img: './logos/innogital.png'
+})
+
+function renderProducts(arr){
+    for (productos of arr){
+        const productCard = document.createElement('div');
+        productCard.classList.add('product');
+
+        const img = document.createElement('img');
+        img.setAttribute('src', productos.img);
+
+        const info = document.createElement('div');
+        info.classList.add('info');
+
+        const productDiv = document.createElement('div');
+
+        const productCost = document.createElement('p');
+        productCost.innerText = '$' + productos.price;
+        
+        const productName = document.createElement('p');
+        productName.innerText = productos.name;
+        
+        
+        const figure = document.createElement('figure');
+        const imgCar = document.createElement('img');
+        imgCar.setAttribute('src', './icons/bt_add_to_cart.svg');
+        
+        productDiv.append(productCost, productName);
+        figure.appendChild(imgCar);
+        info.appendChild(productDiv);
+        info.appendChild(figure);
+        productCard.appendChild(img);
+        productCard.appendChild(info);
+
+        cards.appendChild(productCard);
+    }
+}
+
+renderProducts(productsList)
